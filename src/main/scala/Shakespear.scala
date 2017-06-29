06db475e97c0e962632d207054120980e0047c43
Your Name <you@example.com>
2017-06-29 16:08:34 +0800

    no optimization: ~240s

--- src/main/scala/Shakespear.scala
+++ src/main/scala/Shakespear.scala
@@ -1,8 +1,12 @@
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.mapred.{TextInputFormat, TextOutputFormat}
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
+
 
 /**
   * Created by Administrator on 2017/6/26.
@@ -18,28 +22,40 @@ object Shakespear {
 
     val startTime = System.currentTimeMillis()
 
-    val inputFile = sc.textFile(inputPath).persist(StorageLevel.MEMORY_ONLY)
+    val numPartition = 15
+    val inputFile = sc.textFile(inputPath)
+      .persist(StorageLevel.MEMORY_ONLY).
+      coalesce(numPartition, false)
+
+//    val inputFile = sc.newAPIHadoopFile[Text, LongWritable, CombineTextInputFormat](inputPath)
+//    inputFile.saveAsNewAPIHadoopFile(outputPath, classOf[LongWritable], classOf[Text],classOf[TextOutputFormat[LongWritable,Text]], conf)
+
     val stopwords = sc.broadcast(getStopWords())
 
 //    val blankLineSum = inputFile.filter(line => line == "").count()
     val blankLineSum = sc.accumulator(0)
+
+    val ckpt1 = System.currentTimeMillis()
 
     val res = inputFile.filter(line => {
       if(line == "") {
         blankLineSum.add(1)
         false
       } else true
-    }).flatMap(getWords(_))
-      .filter(wordPair => !stopwords.value.contains(wordPair._2))
-      .map(wordPair => (wordPair._2, (1, wordPair._1)))
-      .reduceByKey((a, b) => (a._1 + b._1, a._2))
-      .map(wordPair => (wordPair._2._2, wordPair._2._1))
+    })
+      .flatMap(getWords(_))
+      .filter(word => !stopwords.value.contains(word.toLowerCase))
+      .map(word => (word, 1))
+      .reduceByKey(_+_)
+//      .map(wordPair => (wordPair._2._2, wordPair._2._1))
       .sortBy(_._2, false).take(100)
 
     val stopTime = System.currentTimeMillis()
 
     println(blankLineSum)
     res.foreach(println(_))
+    println(ckpt1 - startTime)
+    println(stopTime - ckpt1)
     println(stopTime - startTime)
 
     sc.stop()
@@ -48,14 +64,15 @@ object Shakespear {
     def getWords(str: String) =
     {
       val pattern = "[a-zA-Z]+".r
-      pattern.findAllIn(str).toArray.map(word => (word, word.toLowerCase))
+      pattern.findAllIn(str).toArray
+//        .map(word => (word, word.toLowerCase))
     }
 
     def getStopWords() =
     {
       val stopwords = Source.fromInputStream(Shakespear.getClass.getClassLoader.getResourceAsStream("stopword.txt")).getLines()
-        .map(_.trim()).toList
-//      stopwords ::: stopwords.map(_.capitalize) ::: stopwords.map(_.toUpperCase)
+      .map(_.trim()).toList
+      //      stopwords ::: stopwords.map(_.capitalize) ::: stopwords.map(_.toUpperCase)
       stopwords
     }
 
