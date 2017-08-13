dddc6a95dca3e12e5d7c4985f5d944992e97b5f4
Big Dick <Big@Dick.com>
2017-08-13 17:14:47 +0800

    final

--- Spark/src/main/scala/SessionAnalysis/UserFilter.scala
+++ Spark/src/main/scala/SessionAnalysis/UserFilter.scala
@@ -11,8 +11,6 @@ object UserFilter {
 
   def run(sc: SparkContext, input: String, output: String, cond: UserFilterCondition): RDD[(String, Array[String])] =
   {
-    //    val sqlContext = new SQLContext(sc)
-
     val inputPath = { if(DEBUG) "C:\\a\\test\\user.txt" else input }
     val outputPath = { if(DEBUG) "" else output }
     val numProcessors = { if(DEBUG) 4 else 4 }
@@ -25,17 +23,6 @@ object UserFilter {
       .map(array => (array(0), Array(array(1), array(2), array(3), array(4), array(5), array(6))))
     return users
 
-    //    val userRDD = sc.textFile(inputPath, numProcessors)
-
-    /*//Silly. Naive.
-        val schemaString = "user_id username name age profession city sex"
-        val fields = schemaString.split(" ")
-          .map(fieldName => StructField(fieldName, StringType, nullable = true))
-        val schema = StructType(fields)
-
-        val rowRDD = userRDD.map(_.split(" "))
-          .map(record => Row(record(0), record(1), record(2), record(3), record(4), record(5)))
-    */
   }
 
 }
